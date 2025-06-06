fb51114509a7d8c10c5525f4ca789ab93f750f55
Albert Olszewski <albertolszewski@Mac.lan1>
2025-06-05 21:27:24 -0400

    First pass at writing functions

--- m1/README.md
+++ m1/README.md
@@ -1,1 +1,9 @@
-/* Add Author and Project Details here */
+/* 
+
+Author: Albert Olszewski
+Assignment: Introduction to Embedded Systems Programming and Development Environments
+
+This repo contains a couple of functions that can analyze an array of unsigned char data items and report analytics on the maximum, minimum, mean, and median of the data set. 
+In addition, the data set is reordered from large to small. All statistics are rounded down to the nearest integer. After analysis and sorting is done, the data is printed to the screen in a nicely formatted presentation. 
+
+*/
--- m1/stats.h
+++ m1/stats.h
@@ -7,34 +7,84 @@
  * software. Alex Fosdick and the University of Colorado are not liable for any
  * misuse of this material. 
  *
- *****************************************************************************/
-/**
- * @file <Add File Name> 
- * @brief <Add Brief Description Here >
+ * @file     stats.h
+ * @brief    Header file for statistics functions
  *
- * <Add Extended Description Here>
+ * Author:   Albert Olszewski
+ * Date:     2025-06-05
  *
- * @author <Add FirsName LastName>
- * @date <Add date >
- *
- */
+ * Description:
+ * This header file contains the declarations for a set of functions that perform
+ * statistical analysis on an array of unsigned char data, such as calculating
+ * the mean, median, minimum, and maximum values, as well as sorting and printing
+ * the array and its statistics.
+ ******************************************************************************/
+
 #ifndef __STATS_H__
 #define __STATS_H__
 
-/* Add Your Declarations and Function Comments here */ 
+/**
+ * @brief Prints the statistics of an array (minimum, maximum, mean, median)
+ *
+ * @param data Pointer to the array of data
+ * @param size The number of elements in the array
+ */
+void print_statistics(unsigned char *data, unsigned int size);
 
 /**
- * @brief <Add Brief Description of Function Here>
+ * @brief Prints the elements of the array
  *
- * <Add Extended Description Here>
- *
- * @param <Add InputName> <add description here>
- * @param <Add InputName> <add description here>
- * @param <Add InputName> <add description here>
- * @param <Add InputName> <add description here>
- *
- * @return <Add Return Informaiton here>
+ * @param data Pointer to the array of data
+ * @param size The number of elements in the array
  */
+void print_array(unsigned char *data, unsigned int size);
 
+/**
+ * @brief Calculates and returns the median value of the array
+ *
+ * @param data Pointer to the array of data
+ * @param size The number of elements in the array
+ *
+ * @return The median value of the array
+ */
+unsigned char find_median(unsigned char *data, unsigned int size);
+
+/**
+ * @brief Calculates and returns the mean value of the array
+ *
+ * @param data Pointer to the array of data
+ * @param size The number of elements in the array
+ *
+ * @return The mean value of the array
+ */
+unsigned char find_mean(unsigned char *data, unsigned int size);
+
+/**
+ * @brief Finds and returns the maximum value in the array
+ *
+ * @param data Pointer to the array of data
+ * @param size The number of elements in the array
+ *
+ * @return The maximum value in the array
+ */
+unsigned char find_maximum(unsigned char *data, unsigned int size);
+
+/**
+ * @brief Finds and returns the minimum value in the array
+ *
+ * @param data Pointer to the array of data
+ * @param size The number of elements in the array
+ *
+ * @return The minimum value in the array
+ */
+unsigned char find_minimum(unsigned char *data, unsigned int size);
+
+/**
+ * @brief Sorts the array from largest to smallest (in-place)
+ *
+ * @param data Pointer to the array of data
+ * @param size The number of elements in the array
+ */
+void sort_array(unsigned char *data, unsigned int size);
 
 #endif /* __STATS_H__ */
